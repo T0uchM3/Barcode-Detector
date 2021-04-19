6d2d61958f57165a37320831e1ed5f2c96562011
T0UCHM3 <adolf.hazmi@yahoo.com>
2021-04-20 00:23:09 +0100

    Successful first test
    Can read and decode the barcode

--- Barcode-Detector/Barcode_Detector.py
+++ Barcode-Detector/Barcode_Detector.py
@@ -1,10 +1,25 @@
+#import cv2
+#cap = cv2.VideoCapture(0)
+#while(cap.isOpened()):
+# ret, frame = cap.read()
+# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# cv2.imshow('frame',gray)
+# if cv2.waitKey(1) & 0xFF == ord('q'):
+#    break
+#cap.release()
+#cv2.destroyAllWindows()
+
+
+#import pyzbar
+#import argparse
 import cv2
-cap = cv2.VideoCapture(0)
-while(cap.isOpened()):
- ret, frame = cap.read()
- gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- cv2.imshow('frame',gray)
- if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
-cap.release()
-cv2.destroyAllWindows()
+#import numpy as np
+
+
+import pyzbar.pyzbar as zbar
+from PIL import Image
+image = cv2.imread("img2.jpg")
+
+#barcode = zbar.decode(Image.open("img2.png"))
+barcode = zbar.decode(image)
+print('ZBar: {}'.format(barcode[0].data.decode("utf-8")))
