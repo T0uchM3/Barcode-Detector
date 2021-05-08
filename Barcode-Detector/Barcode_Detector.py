fffb94d0a3a5c02cdd2685f4b2431c0c02650d38
T0UCHM3 <adolf.hazmi@yahoo.com>
2021-05-08 01:20:42 +0100

    Major part of the UI has been made
    Going frameless with the window, using "CustomizeWindowHint" flag for resizing with custom dragging from almost all points in the window

--- Barcode-Detector/Barcode_Detector.py
+++ Barcode-Detector/Barcode_Detector.py
@@ -1,26 +1,136 @@
+##import cv2
+##cap = cv2.VideoCapture(0)
+##while(cap.isOpened()):
+## ret, frame = cap.read()
+## gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+## cv2.imshow('frame',gray)
+## if cv2.waitKey(1) & 0xFF == ord('q'):
+##    break
+##cap.release()
+##cv2.destroyAllWindows()
+
+
+##import pyzbar
+##import argparse
 #import cv2
-#cap = cv2.VideoCapture(0)
-#while(cap.isOpened()):
-# ret, frame = cap.read()
-# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('frame',gray)
-# if cv2.waitKey(1) & 0xFF == ord('q'):
-#    break
-#cap.release()
-#cv2.destroyAllWindows()
+##import numpy as np
 
 
-#import pyzbar
-#import argparse
-import cv2
-#import numpy as np
+#import pyzbar.pyzbar as zbar
+#from PIL import Image
+#image = cv2.imread("img2.jpg")
+
+##barcode = zbar.decode(Image.open("img2.png"))
+#barcode = zbar.decode(image)
+#print('ZBar: {}'.format(barcode[0].data.decode("utf-8")))
 
 
-import pyzbar.pyzbar as zbar
-from PIL import Image
-image = cv2.imread("img2.jpg")
 
-#barcode = zbar.decode(Image.open("img2.png"))
-barcode = zbar.decode(image)
-print('ZBar: {}'.format(barcode[0].data.decode("utf-8")))
 
+import sys
+
+from PyQt5.QtCore import QPoint
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
+
+
+
+class MainWindow(QWidget):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.layout  = QVBoxLayout()
+        self.layout.addWidget(MyBar(self))
+        self.setLayout(self.layout)
+        self.layout.setContentsMargins(0,0,0,0)
+        self.layout.addStretch(-1)
+        self.setMinimumSize(800,400)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.pressing = False
+
+
+class MyBar(QWidget):
+
+    def __init__(self, parent):
+        super(MyBar, self).__init__()
+        self.parent = parent
+        print(self.parent.width())
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(0,0,0,0)
+        self.title = QLabel("My Own Bar")
+
+        btn_size = 35
+
+        self.btn_close = QPushButton("x")
+        self.btn_close.clicked.connect(self.btn_close_clicked)
+        self.btn_close.setFixedSize(btn_size,btn_size)
+        self.btn_close.setStyleSheet("background-color: red;")
+
+        self.btn_min = QPushButton("-")
+        self.btn_min.clicked.connect(self.btn_min_clicked)
+        self.btn_min.setFixedSize(btn_size, btn_size)
+        self.btn_min.setStyleSheet("background-color: gray;")
+
+        self.btn_max = QPushButton("+")
+        self.btn_max.clicked.connect(self.btn_max_clicked)
+        self.btn_max.setFixedSize(btn_size, btn_size)
+        self.btn_max.setStyleSheet("background-color: gray;")
+
+        self.title.setFixedHeight(35)
+        self.title.setAlignment(Qt.AlignCenter)
+        self.layout.addWidget(self.title)
+        self.layout.addWidget(self.btn_min)
+        self.layout.addWidget(self.btn_max)
+        self.layout.addWidget(self.btn_close)
+
+        self.title.setStyleSheet("""
+            background-color: black;
+            color: white;
+        """)
+        self.setLayout(self.layout)
+
+        self.start = QPoint(0, 0)
+        self.pressing = False
+
+    def resizeEvent(self, QResizeEvent):
+        super(MyBar, self).resizeEvent(QResizeEvent)
+        self.title.setFixedWidth(self.parent.width())
+
+    def mousePressEvent(self, event):
+        self.start = self.mapToGlobal(event.pos())
+        self.pressing = True
+
+    def mouseMoveEvent(self, event):
+        if self.pressing:
+            self.end = self.mapToGlobal(event.pos())
+            self.movement = self.end-self.start
+            self.parent.setGeometry(self.mapToGlobal(self.movement).x(),
+                                self.mapToGlobal(self.movement).y(),
+                                self.parent.width(),
+                                self.parent.height())
+            self.start = self.end
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.pressing = False
+
+
+    def btn_close_clicked(self):
+        self.parent.close()
+
+    def btn_max_clicked(self):
+        self.parent.showMaximized()
+
+    def btn_min_clicked(self):
+        self.parent.showMinimized()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mw = MainWindow()
+    mw.show()
+    sys.exit(app.exec_())
--- Barcode-Detector/guiTest.py
+++ Barcode-Detector/guiTest.py
@@ -1,60 +1,301 @@
-#import sys
-
-## 1. Import `QApplication` and all the required widgets
-#from PyQt5.QtWidgets import QApplication
-#from PyQt5.QtWidgets import QLabel
-#from PyQt5.QtWidgets import QWidget
-#app = QApplication(sys.argv)
-## 3. Create an instance of your application's GUI
-#window = QWidget()
-#window.setWindowTitle('PyQt5 App')
-#window.setGeometry(100, 100, 280, 80)
-#window.move(60, 15)
-#helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-#helloMsg.move(60, 15)
-
-## 4. Show your application's GUI
-#window.show()
-
-## 5. Run your application's event loop (or main loop)
-#sys.exit(app.exec_())
-
-
-# This Python file uses the following encoding: utf-8
 import sys
 import os
-
-
+import cv2
 from PySide2.QtWidgets import QApplication, QWidget
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtCore import QFile
+from PySide2.QtCore import *
 from PySide2.QtUiTools import QUiLoader
+from PIL import Image
+from PySide2.QtWidgets import *
+import pyzbar.pyzbar as zbar
 
-loader = QUiLoader()
-app = QtWidgets.QApplication(sys.argv)
-window = loader.load("form.ui", None)
+
+class MainWidget(QWidget):
+    
+    
+    def __init__(self):
+
+        QWidget.__init__(self)
+
+        full_path = os.path.realpath(__file__)
+        path, filename = os.path.split(full_path)
+        designer_file = QFile(path + "\\form.ui")
+        designer_file.open(QFile.ReadOnly)
+        self.setWindowFlags(Qt.CustomizeWindowHint)
+        loader = QUiLoader()
+        self.ui = loader.load(designer_file, self)
+        #loader.registerCustomWidget(DragBar)
+        #self.drag = DragBar(self)
+        #self.layout  = QVBoxLayout()
+        #self.layout.addWidget(self.ui.DragBar)
+        #self.setLayout(self.layout)
+        self.center()
+        self.pressed = False
+        
+        #w = QtWidgets.QDialog()
+        #w.setSizeGripEnabled(True)
+        self.ui.closeButton.clicked.connect(self.closeEvent)
+        self.ui.minimizeButton.clicked.connect(self.minimizeEvent)
+        #drag.center(self)
+        #list stuff
+        #for i in range(40):
+        #    self.ui.picList.addItem(str(i))
+
+        #self.ui.splitter.setStretchFactor(1,1)
+        self.ui.splitter.setSizes([200,200])#even split of splitter sides
+
+        #the resize doesn't seems to working without this grid layout
+        grid_layout = QGridLayout() 
+        grid_layout.addWidget(self.ui) 
+        self.setLayout(grid_layout)
+        #grid_layout.setMargin(0)
+        
+        #self.gripSize = 16
+        #self.grips = []
+        #for i in range(4):
+        #    grip = QSizeGrip(self)
+        #    grip.resize(self.gripSize, self.gripSize)
+        #    self.grips.append(grip)
+
+        self.setAcceptDrops(True)
+        designer_file.close()
+        
+        #self._initDrag()  # Set the default value of mouse tracking judgment trigger
+
+        self.setMouseTracking(True)  # Set widget mouse tracking
+        #self.DragBar.installEventFilter(self)  # Initialize event filter
+        #self.DragBar.installEventFilter(self)
+
+
+    def gribSelect(self):
+        print("CLIIII")
+    def closeEvent(self):
+        self.close()
+    def minimizeEvent(self):
+        self.showMinimized()
+    def dragEnterEvent(self, e):
+        """
+        This function will detect the drag enter event from the mouse on the main window
+        """
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+    def dropEvent(self,e):
+        """
+        This function will enable the drop file directly on to the
+        main window. The file location will be stored in the self.filename
+        """
+        for url in e.mimeData().urls():
+            fname = str(url.toLocalFile())
+            self.ui.picList.addItem(str(os.path.basename(fname)))
+        self.filename = fname
+        print("ADR ", self.filename)
+        
+        image = cv2.imread(self.filename)
+        barcode = zbar.decode(image)
+        print('ZBar: {}'.format(barcode[0].data.decode("utf-8")))
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+        self.pressed =True
+
+    def mouseMoveEvent(self, event):
+        if self.pressed:
+            delta = QPoint (event.globalPos() - self.oldPos)
+            self.move(self.x() + delta.x(), self.y() + delta.y())
+            self.oldPos = event.globalPos()
+
+    def mouseReleaseEvent(self, event):
+        self.pressed = False
+        print("release")
+    def resizeEvent(self, event):
+        print("REEEEEEE")
+        QMainWindow.resizeEvent(self, event)
+        rect = self.rect()
+        # top left grip doesn't need to be moved...
+        # top right
+        self.grips[1].move(rect.right() - self.gripSize, 0)
+        # bottom right
+        self.grips[2].move(
+            rect.right() - self.gripSize, rect.bottom() - self.gripSize)
+        # bottom left
+        self.grips[3].move(0, rect.bottom() - self.gripSize)
+class DragBar(QWidget):
+    #p = MainWidget
+    def __init__(self, parent):
+        #QWidget.__init__(self, parent)
+        print("test")
+        print("parent",str(parent))
+        super(DragBar, self).__init__()
+        
+        self.parent = parent
+        #QWidget.__init__(self, parent)
+        #self.center(parent)
+        global p
+        #p = parent
+        #self.isPressed = False
+        self.start = QPoint(0, 0)
+        self.pressing = False
+    #def center(self,parent):
+    #    qr = parent.frameGeometry()
+    #    cp = QDesktopWidget().availableGeometry().center()
+    #    qr.moveCenter(cp)
+    #    parent.move(qr.topLeft())
+    #def mouseReleaseEvent(self, event):
+    #    self.isPressed = False
+    #def mousePressEvent(self, event):
+    #    self.isPressed = True
+    #    #self.oldPos = event.globalPos()
+    #    self.startMovePos = event.globalPos()
+
+    #def mouseMoveEvent(self, event):
+        #delta = QPoint (event.globalPos() - p.oldPos)
+        #p.move(p.x() + delta.x(), p.y() + delta.y())
+        #p.oldPos = event.globalPos()
+
+
+        #mainWin = self.parent().parent().parent()
+        #mainWinPos = mainWin.pos()
+        #delta = QPoint (event.globalPos() - self.oldPos)
+        #mainWin.move(mainWinPos.x() + delta.x(), mainWinPos.y() + delta.y())
+
+
+         #if self.isPressed:
+         #   movePoint = event.globalPos() - self.startMovePos
+         #   widgetPos = self.parentWidget().parentWidget().parentWidget().pos()
+         #   self.startMovePos = event.globalPos()
+         #   self.parentWidget().parentWidget().parentWidget().move(widgetPos.x() + movePoint.x(), widgetPos.y() + movePoint.y())
+
+
+
+
+    def mousePressEvent(self, event):
+        self.parent.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        delta = QPoint (event.globalPos() - self.parent.oldPos)
+        self.parent.move(self.parent.x() + delta.x(), self.parent.y() + delta.y())
+        self.parent.oldPos = event.globalPos()
+
+
+
+    #def mousePressEvent(self, event):
+    #    self.start = self.mapToGlobal(event.pos())
+    #    self.pressing = True
+    #    print("PRESSSSED")
+
+    #def mouseMoveEvent(self, event):
+    #    if self.pressing:
+    #        self.end = self.mapToGlobal(event.pos())
+    #        self.movement = self.end-self.start
+    #        #mainWin = self.parent().parent()
+    #        #print(str(mainWin))
+    #        self.parent.setGeometry(self.mapToGlobal(self.movement).x(),
+    #                            self.mapToGlobal(self.movement).y(),
+    #                            self.parent.width(),
+    #                            self.parent.height())
+    #        self.start = self.end
+
+    #def mouseReleaseEvent(self, QMouseEvent):
+    #    self.pressing = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #def resizeEvent(self, QResizeEvent):
+    ## Custom window sizing events
+    ## Change the window size by three coordinate ranges
+    #    self._right_rect = [QPoint(x, y) for x in range(self.width() - 5, self.width() + 5)
+    #                        for y in range(self.widget.height() + 20, self.height() - 5)]
+    #    self._bottom_rect = [QPoint(x, y) for x in range(1, self.width() - 5)
+    #                         for y in range(self.height() - 5, self.height() + 1)]
+    #    self._corner_rect = [QPoint(x, y) for x in range(self.width() - 5, self.width() + 1)
+    #                     for y in range(self.height() - 5, self.height() + 1)]
+
+    #def mousePressEvent(self, event):
+    #    # Override mouse click events
+    #    if (event.button() == Qt.LeftButton) and (event.pos() in self._corner_rect):
+    #        # Left click the border area in the lower right corner
+    #        self._corner_drag = True
+    #        event.accept()
+    #    elif (event.button() == Qt.LeftButton) and (event.pos() in self._right_rect):
+    #        # Left click on the right border area
+    #        self._right_drag = True
+    #        event.accept()
+    #    elif (event.button() == Qt.LeftButton) and (event.pos() in self._bottom_rect):
+    #        # Click the lower border area with the left mouse button
+    #        self._bottom_drag = True
+    #        event.accept()
+    #    elif (event.button() == Qt.LeftButton) and (event.y() < self.widget.height()):
+    #        # Left click on the title bar area
+    #        self._move_drag = True
+    #        self.move_DragPosition = event.globalPos() - self.pos()
+    #        event.accept()
+
+    #def mouseMoveEvent(self, QMouseEvent):
+    #    # Determine mouse position and switch mouse gesture
+    #    if QMouseEvent.pos() in self._corner_rect:  # QMouseEvent.pos() get relative position
+    #        self.setCursor(Qt.SizeFDiagCursor)
+    #    elif QMouseEvent.pos() in self._bottom_rect:
+    #        self.setCursor(Qt.SizeVerCursor)
+    #    elif QMouseEvent.pos() in self._right_rect:
+    #        self.setCursor(Qt.SizeHorCursor)
+
+    #    # When the left mouse button click and meet the requirements of the click area, different window adjustments are realized
+    #    # There is no definition of the left and top five directions, mainly because the implementation is not difficult, but the effect is very poor. When dragging and dropping, the window flickers, and then study whether there is a better implementation
+    #    if Qt.LeftButton and self._right_drag:
+    #        # Right adjust window width
+    #        self.resize(QMouseEvent.pos().x(), self.height())
+    #        QMouseEvent.accept()
+    #    elif Qt.LeftButton and self._bottom_drag:
+    #        # Lower adjustment window height
+    #        self.resize(self.width(), QMouseEvent.pos().y())
+    #        QMouseEvent.accept()
+    #    elif Qt.LeftButton and self._corner_drag:
+    #        #  Since I used the rounded corners, I didn't have to adjust the size of the window
+    #        # Adjust the height and width at the same time in the lower right corner
+    #        self.resize(QMouseEvent.pos().x(), QMouseEvent.pos().y())
+    #        QMouseEvent.accept()
+    #    elif Qt.LeftButton and self._move_drag:
+    #        # Title bar drag and drop window position
+    #        self.move(QMouseEvent.globalPos() - self.move_DragPosition)
+    #        QMouseEvent.accept()
+
+    #def mouseReleaseEvent(self, QMouseEvent):
+    ## After the mouse is released, each trigger is reset
+    #    self._move_drag = False
+    #    self._corner_drag = False
+    #    self._bottom_drag = False
+    #    self._right_drag = False
+    #def _initDrag(self):
+    #    # Set the default value of mouse tracking judgment trigger
+    #    self._move_drag = False
+    #    self._corner_drag = False
+    #    self._bottom_drag = False
+    #    self._right_drag = False
+
+app = QApplication([])
+window = MainWidget()
 window.show()
 app.exec_()
 
-
-
-
-#class test(QWidget):
-#    def __init__(self):
-#        super(test, self).__init__()
-#        self.load_ui()
-
-#    def load_ui(self):
-#        loader = QUiLoader()
-#        path = os.path.join(os.path.dirname(__file__), "form.ui")
-#        ui_file = QFile(path)
-#        ui_file.open(QFile.ReadOnly)
-#        loader.load(ui_file, self)
-#        ui_file.close()
-
-#if __name__ == "__main__":
-#    app = QApplication([])
-#    widget = test()
-#    widget.show()
-#    sys.exit(app.exec_())
