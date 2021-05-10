87ad6ebe1e678d2acb02313ee56e4e4fb7543bf4
T0UCHM3 <adolf.hazmi@yahoo.com>
2021-05-11 00:47:10 +0100

    Able to read barcodes from local images and video
    First step, still needs more polishing and fixing

--- Barcode-Detector/guiTest.py
+++ Barcode-Detector/guiTest.py
@@ -9,9 +9,15 @@ from PySide2.QtGui import *
 from PySide2.QtUiTools import QUiLoader
 from PIL import Image
 from PySide2.QtWidgets import *
+from pyzbar import pyzbar
 import pyzbar.pyzbar as zbar
+import numpy as np
 
-class MainWidget(QWidget):    
+class MainWidget(QWidget):   
+    imageList=[]
+    codeList=[]
+    noneInside = True
+    #mapSingnal = Signal(np.ndarray)
     def __init__(self):
         QWidget.__init__(self)
 
@@ -25,29 +31,134 @@ class MainWidget(QWidget):
         self.ui = loader.load(designer_file, self)
         self.center()#position the window in the center
         
+        global codeList
+        self.codeList = []
+        #global imageList
+        #self.imageList = QtGui.QDrag(self)
+
         #w = QtWidgets.QDialog()
         #w.setSizeGripEnabled(True)
         self.ui.closeButton.clicked.connect(self.closeEvent)
         self.ui.minimizeButton.clicked.connect(self.minimizeEvent)
-
+        self.ui.picList.currentItemChanged.connect(self.selectionChanged)
+        self.ui.vidButton.clicked.connect(self.readVideo)
+        self.ui.picButton.clicked.connect(self.testMeth)
+        #self.mapSingnal.connect(self.displayVideo)
         #list stuff
         #for i in range(40):
         #    self.ui.picList.addItem(str(i))
 
-        #self.ui.splitter.setStretchFactor(1,1)
-        self.ui.splitter.setSizes([200,200])#even split of splitter sides
+        #self.ui.splitter.setStretchFactor(1,0)
+        self.ui.splitter.setSizes([500,600])#splitting the splitter's sides??
 
         #the resize doesn't seems to working without this grid layout
         grid_layout = QGridLayout() 
         grid_layout.addWidget(self.ui) 
         self.setLayout(grid_layout)
-        #grid_layout.setMargin(0)
+        grid_layout.setMargin(0)
 
         self.setAcceptDrops(True)
         designer_file.close()
 
         self.setMouseTracking(True)  # Set widget mouse tracking
         self.pressed = False
+        
+    def testMeth(self):
+        print("len ", len(self.codeList))
+        for obj in self.codeList:
+            print( obj.code )
+    def displayVideo(self, image):
+        print("displayVid")
+        pixmap = QPixmap.fromImage(image)
+        self.ui.picDisplayer.setPixmap(pixmap)
+
+        #barcodes = zbar.decode(image)
+
+        #barcodes = pyzbar.decode(image)
+        #for barcode in barcodes:
+        #   barcodeData = barcode.data.decode("utf-8")
+        #   barcodeType = barcode.type
+        #   text = "{} ({})".format(barcodeData, barcodeType)
+        #   print(text)
+
+            #print('ZBar: {}'.format(barcode[0].data.decode("utf-8")))
+        #image2 = cv2.imread(image)
+        #barcode = zbar.decode(image2)
+        #print('ZBar: {}'.format(barcode[0].data.decode("utf-8")))
+    def readVideo(self):
+        cap = cv2.VideoCapture('barcodeVid2.mp4')
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == False:
+                break
+            #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            #cv2.imshow('frame',frame)
+            #self.mapSingnal.emit(frame)
+            image = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+            #self.displayVideo(image)
+            barcodes = pyzbar.decode(frame)
+            for barcode in barcodes:
+               barcodeData = barcode.data.decode("utf-8")
+               barcodeType = barcode.type
+               text = "{} ({})".format(barcodeData, barcodeType)
+               print(text)
+               for obj in self.codeList:
+                   if obj.code == barcodeData:
+                       self.noneInside=False
+                   else:
+                       self.noneInside=True
+               if self.noneInside:
+                   self.codeList.append( codes(barcodeData,barcodeType) )
+               
+               
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+    def selectionChanged(self):
+        """ Detect item selection change inside the UI list """
+        for str in self.imageList:
+            if self.ui.picList.currentItem().text() in str:
+                pixmap = QtGui.QPixmap(str)
+                self.ui.picDisplayer.setPixmap(pixmap)
+                print(self.ui.picList.currentItem().text())
+                print(str)
+         #for url in self.imageList.mimeData().urls():
+            #print("select", self.ui.picList.currentItem().text())
+            
+            #print(str(url.toLcalFile()))
+                image = cv2.imread(str)
+                barcode = zbar.decode(image)
+                print('ZBar: {}'.format(barcode[0].data.decode("utf-8")))
+    def dragEnterEvent(self, e):
+        """
+        This function will detect the drag enter event from the mouse on the main window
+        Like it open the window to accept file dropping
+        """
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+    def dropEvent(self,e):
+        """
+        This function will enable the drop file(s) directly on to the
+        main window. The file location will be stored in the self.filename
+        """
+
+        #self.imageList.setMimeData(e.mimeData())
+        for url in e.mimeData().urls():
+            fname = str(url.toLocalFile())
+            self.ui.picList.addItem(str(os.path.basename(fname)))
+            print("path",fname)
+            self.imageList.append(fname)
+        self.filename = fname
+        print("ADR ", self.filename)
+        
+        #image = cv2.imread(self.filename)
+        #barcode = zbar.decode(image)
+        #print('ZBar: {}'.format(barcode[0].data.decode("utf-8")))
+
 
     def closeEvent(self):
         self.close()
@@ -66,13 +177,19 @@ class MainWidget(QWidget):
 
     def mouseMoveEvent(self, event):
         if self.pressed:
+            self.setWindowOpacity(0.8)
             delta = QPoint (event.globalPos() - self.oldPos)
             self.move(self.x() + delta.x(), self.y() + delta.y())
             self.oldPos = event.globalPos()
 
     def mouseReleaseEvent(self, event):
+        self.setWindowOpacity(1)
         self.pressed = False
         print("release")
+class codes:
+    def __init__(self, code, type):
+        self.code = code
+        self.type = type
 
 app = QApplication([])
 window = MainWidget()
